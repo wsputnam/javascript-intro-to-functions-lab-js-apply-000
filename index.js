e0becc49d8aa3c18f7a95c203b157ce73e405c9d
William Putnam <williamputnamjr@gmail.com>
2017-10-10 22:00:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  } else if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!';
+  } else if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  }
+  
+}
